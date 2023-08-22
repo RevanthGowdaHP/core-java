739d4054e0fb7da9266f941e5c34ca9980f9f12c
RevanthGowdaHP <revanthgowdahp.xworkz@gmail.com>
2023-08-22 11:49:19 +0530

    examples for arrays

--- BPMonitor .java
+++
@@ -1,9 +0,0 @@
-class BPMonitor 
-{
-public static void main(String arg[])
-{
- System.out.println("Blood Pressure MonitorBP Monitor Device");
-System.out.println("Battery");
-System.out.println("Pulse Rate Indicator");
-System.out.println("Memory Function");
-System.out.println("Movement Indicator");}}
