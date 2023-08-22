d6e559c4e47c30ad1d9273a7116637d8fbc8f3f8
RevanthGowdaHP <revanthgowdahp.xworkz@gmail.com>
2023-08-22 11:41:45 +0530

    examples related to variables and datatypes

---
+++ BPMonitor .java
@@ -0,0 +1,9 @@
+class BPMonitor 
+{
+public static void main(String arg[])
+{
+ System.out.println("Blood Pressure MonitorBP Monitor Device");
+System.out.println("Battery");
+System.out.println("Pulse Rate Indicator");
+System.out.println("Memory Function");
+System.out.println("Movement Indicator");}}
