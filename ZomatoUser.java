70a2a2961b4d108f7bbd84c4bc1d4e2f7374d7aa
RevanthGowdaHP <revanthgowdahp.xworkz@gmail.com>
2023-08-22 11:58:12 +0530

    examples for Method overloading

--- ZomatoUser.java
+++ ZomatoUser.java
@@ -1,11 +1,8 @@
 class ZomatoUser{
 public static void main(String zoma[]){
 
-double amount = Zomato.search("Pizza");
-System.out.println("price of the Pizza "+"Rs :"+amount);
- double quantityAmount = Zomato.search("Pizza",5);
-System.out.println("Price of the Pizza with Quanitity ="+quantityAmount);
- 
+double amount = Tomato.search("Pizza");
+System.out.println("price of the Pizza :"+amount);
 
 }
 }
