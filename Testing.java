cdb802d257742a7383b0f296ad613907f3234436
RevanthGowdaHP <revanthgowdahp.xworkz@gmail.com>
2023-08-23 20:20:55 +0530

    methods examples

---
+++ Testing.java
@@ -0,0 +1,23 @@
+class Testing{
+
+public static void main(String srg[]){
+
+ Speaker.onOrOff();
+ Speaker.maxVolume();
+  Speaker.maxVolume();
+   Speaker.maxVolume();
+    Speaker.maxVolume();
+	 Speaker.maxVolume();
+	  Speaker.maxVolume();
+	 Speaker.minVolume();
+	 Speaker.minVolume();
+	 Speaker.minVolume();
+	 Speaker.minVolume();
+	  Speaker.minVolume();
+	   Speaker.minVolume();
+ Speaker.onOrOff();
+ Speaker.minVolume();
+Speaker.maxVolume();
+}
+
+}
