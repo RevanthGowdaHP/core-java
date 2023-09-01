c569b3c204a52140e642504c0c7fc7c1f168daff
RevanthGowdaHP <revanthgowdahp.xworkz@gmail.com>
2023-09-01 09:46:51 +0530

    Using Constructor to create object

--- EatSureUser.java
+++
@@ -1,9 +0,0 @@
-class EatSureUser{
-public static void main(String eat[]){
-
-double amount = EatSure.search("Ratatouille");
-System.out.println("price of the Ratatouille :"+amount);
-double quantityAmount = EatSure.search("Ratatouille",8);
-System.out.println("Price of the Ratatouille with Quanitity ="+quantityAmount);
-}
-}
