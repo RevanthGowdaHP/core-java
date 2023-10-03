2acc34e9dbc1e771d2b926ffb23727805463d5a0
RevanthGowdaHP <142565725+RevanthGowdaHP@users.noreply.github.com>
2023-10-03 06:59:52 +0530

    Update ReverseWords.java

--- ReverseWords.java
+++ ReverseWords.java
@@ -9,25 +9,15 @@ public class ReverseWords {
         }
 
         public static String reverseMiddleWord(String input) {
-            // Split the input sentence into words
             String[] words = input.split(" ");
-
-            // Check if there are at least three words in the sentence
             if (words.length < 3) {
-                return input; // Cannot reverse the middle word
+                return input; 
             }
-
-            // Find the index of the middle word
             int middleIndex = words.length / 2;
 
-            // Reverse the characters of the middle word
             String middleWord = words[middleIndex];
             StringBuilder ulta = new StringBuilder(middleWord).reverse();
-
-            // Replace the middle word with the reversed one
             words[middleIndex] = ulta.toString();
-
-            // Join the words back together with spaces
             String output = String.join(" ", words);
 
             return output;
